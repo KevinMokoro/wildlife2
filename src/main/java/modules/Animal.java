bfbebd8804ee7da4e8934c69cd4ffad2c7837e86
KevinMokoro <klosvoke1@gmail.com>
2021-04-29 18:14:15 +0300

    Add a constructor to Animal.java and test it instantiates correctly.

---
+++ src/main/java/modules/Animal.java
@@ -0,0 +1,10 @@
+package modules;
+
+public class Animal extends Animals {
+
+    public Animal(String name) {
+        this.name = name;
+
+    }
+
+}
---
+++ src/test/java/modules/AnimalTest.java
@@ -0,0 +1,17 @@
+package modules;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//public Animal setUpNewAnimal() {
+ //   return new Animal("lion");
+ //       }
+
+public class AnimalTest {
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        Animal testAnimal = new Animal("lion");
+        assertEquals(true,testAnimal instanceof Animal);
+    }
+}
