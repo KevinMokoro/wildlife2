549e930b7a2c01828169beb4a6d4661c4704e0e7
KevinMokoro <klosvoke1@gmail.com>
2021-04-30 23:52:20 +0300

    Add health and age constants to endangered animal.

--- src/main/java/modules/Endangered.java
+++ src/main/java/modules/Endangered.java
@@ -5,9 +5,25 @@ import org.sql2o.Connection;
 import java.util.List;
 
 public class Endangered extends Animals {
+    private String health;
+    private String age;
+
+    public static final String HEALTH_ILL = "ill";
+    public static final String HEALTHY_HEALTHY = "healthy";
+    public static final String HEALTHY_OK = "ok";
+
+    public static final String AGE_BABY = "newborn";
+    public static final String AGE_YOUNG = "young";
+    public static final String AGE_ADULT = "adult";
+
     public Endangered(String name,int sightingId) {
         this.name = name;
         this.sightingId = sightingId;
+      //  this.health = health;
+       // this.age = age;
+
+
+
 
     }
     public static List<Endangered> all() {
@@ -26,5 +42,11 @@ public class Endangered extends Animals {
         }
     }
 
+   // public String getHealth() {
+    //    return health;
+  //  }
 
+  //  public String getAge() {
+  //      return age;
+  //  }
 }
