90ebfa5c18eebbe516294b299e47c47314e917cc
KevinMokoro <klosvoke1@gmail.com>
2021-05-01 12:40:35 +0300

    Add index.hbs and display endangered and animals if in the system.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import modules.Animal;
+import modules.Endangered;
+import modules.Sighting;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -9,6 +12,15 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("sightings", Sighting.all());
+            model.put("animals", Animal.all());
+            model.put("endangered", Endangered.all());
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
 
     }
 
