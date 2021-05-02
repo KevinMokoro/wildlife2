b0ec8e6942dc4148cdb02a8f986490169cb7dbd5
KevinMokoro <klosvoke1@gmail.com>
2021-05-03 02:45:45 +0300

    Modify route to animal details and the detail handlebar itself.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -96,6 +96,18 @@ public class App {
             return null;
         });
 
+        get("/animal/:id", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            Animal animal = Animal.find(Integer.parseInt(request.params("id")));
+            model.put("animal", animal);
+            return new ModelAndView(model, "animal-details.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+
+
 
 
 
