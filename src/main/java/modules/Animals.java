d4c04352d64b21999ce2415f05cc12065d80020f
KevinMokoro <klosvoke1@gmail.com>
2021-05-02 17:58:43 +0300

    Modify abstract class animals by removing impossible similarities.

--- src/main/java/modules/Animals.java
+++ src/main/java/modules/Animals.java
@@ -5,61 +5,20 @@ import org.sql2o.*;
 
 public abstract class Animals{
     public String name;
-    public int id;
-    public int sightingId;
     public Timestamp createdAt;
-   // public boolean endangered;
-    public String type;
 
 
     public String getName() {
         return name;
     }
-
-    public int getId() {
-        return id;
-    }
-    public int getSightingId(){
-        return sightingId;
-    }
     public Timestamp getCreatedAt(){
         return createdAt;
     }
 
-   // public boolean isEndangered() {
-   //     return endangered;
-   /// }
 
-    @Override
-    public boolean equals(Object otherAnimal){
-        if (!(otherAnimal instanceof Animals)) {
-            return false;
-        } else {
-            Animals newAnimal = (Animals) otherAnimal;
-            return this.getName().equals(newAnimal.getName()) &&
-                    this.getSightingId() == newAnimal.getSightingId();
-        }
-    }
-    public void save() {
-        try(Connection con = DB.sql2o.open()) {
-            String sql = "INSERT INTO animals (name, sightingId, createdAt, type) VALUES (:name, :sightingId, now(), :type)";
-            this.id = (int) con.createQuery(sql, true)
-                    .addParameter("name", this.name)
-                    .addParameter("sightingId", this.sightingId)
-                    .addParameter("type",this.type)
-                    .executeUpdate()
-                    .getKey();
-        }
-    }
 
-    public void delete() {
-        try(Connection con = DB.sql2o.open()) {
-            String sql = "DELETE FROM animals WHERE id = :id;";
-            con.createQuery(sql)
-                    .addParameter("id", this.id)
-                    .executeUpdate();
-        }
-    }
+
+
 
 
 }
