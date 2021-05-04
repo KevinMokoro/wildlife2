48c984a8e2e7e2b7e7c75fabdf1766b05e7f4568
KevinMokoro <klosvoke1@gmail.com>
2021-05-04 03:02:50 +0300

    Alter completely DB.java so as to be able to push local database to heroku.

--- src/main/java/modules/DB.java
+++ src/main/java/modules/DB.java
@@ -1,14 +1,31 @@
 package modules;
 
 import org.sql2o.*;
-
-//public class DB {
- //   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "kevin", "  ");
-
-//}
-
-
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 public class DB {
-    public static Sql2o sql2o = new Sql2o("jdbc:postresql://ec2-34-225-103-117.compute-1.amazonaws.com:5432/df5sg5ch5k2jrk", "ysrlzbfnjgqjbz", "57766f3caf31e493ed098fce572c7106520090fa37f84eefc55106e2857f72e2");
-}
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
 
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "kevin" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "  " : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e) {
+            logger.error("Unable to connect to database.");
+        }
+    }
+}
